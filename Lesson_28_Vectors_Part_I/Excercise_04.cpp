57d6dcbdcc5bd533da97e215f50c6e764495ebb5
Manar Alwahedi <alwmanar@gmail.com>
2026-02-05 09:09:06 +0300

    Add files via upload

---
+++ Lesson_28_Vectors_Part_I/Excercise_04.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct stEmployee {
+	string FirsName;
+	string LastName;
+	float Salary;
+};
+
+
+int main() {
+
+	vector <stEmployee> vEmployees;
+
+	stEmployee tempEmployee;
+
+	tempEmployee.FirsName = "Mohammed";
+	tempEmployee.LastName = "Ahmed";
+	tempEmployee.Salary = 4500;
+	vEmployees.push_back(tempEmployee);
+
+	tempEmployee.FirsName = "Sara";
+	tempEmployee.LastName = "Omar";
+	tempEmployee.Salary = 6000;
+	vEmployees.push_back(tempEmployee);
+
+	tempEmployee.FirsName = "Sami";
+	tempEmployee.LastName = "Ahmed";
+	tempEmployee.Salary = 8000;
+	vEmployees.push_back(tempEmployee);
+
+	for (stEmployee& E : vEmployees) {
+
+		cout << "First Name = " << E.FirsName << endl;
+		cout << "Salary = " << E.Salary << endl;
+		cout << "\n";
+
+	}
+
+
+}
