c03cc91037cee3fc810fd91f9eb2cfde02040c11
Manar Alwahedi <alwmanar@gmail.com>
2026-02-16 22:41:04 +0300

    Add files via upload

---
+++ Lesson_31_Dynamic_Memory_Allocation/‏‏Excercise_02.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int NumOfStudent;
+	cout << "How manay student do you want to enter ? ";
+	cin >> NumOfStudent;
+
+	float* ptr;
+
+	ptr = new float[NumOfStudent];
+
+	cout << "\nGrade of Students:\n";
+
+	for (int i = 0; i < NumOfStudent; i++)
+	{
+		cout << "Student " << i + 1 << ": ";
+		cin >> *(ptr + i);
+	}
+
+	for (int i = 0; i < NumOfStudent; i++)
+	{
+		cout << "Student " << i + 1 << ": " << *(ptr + i) << endl;
+	}
+
+
+}
